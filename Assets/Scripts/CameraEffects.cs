40bcc8d1318762973879c12d033ea7aa8705c8a6
EchoReid <echoreid43@gmail.com>
2026-02-19 18:47:24 -0800

    Added Camera Shake
    Created this camera shake script as requested.
    
    Tested it using Invoke() within Awake, the camera shake's magnitude is additive if the player takes damage again while camera shake is still going.

---
+++ Assets/Scripts/CameraEffects.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraEffects : MonoBehaviour
+{
+    private Vector3 initialCameraPosition;
+    private float currentMagnitude;
+
+
+    public void CameraShake(float magnitude)
+    {
+        //advice: for every 1 damage taken, the magnitude should probably be 0.1f or 0.05f
+        StartCoroutine(ShakeRoutine(magnitude));
+    }
+
+    private IEnumerator ShakeRoutine(float magnitude)
+    {
+
+        currentMagnitude += magnitude;
+        while (currentMagnitude > 0)
+        {
+            transform.position = initialCameraPosition + Random.insideUnitSphere * currentMagnitude;
+            currentMagnitude -= 0.0001f;
+            yield return null;
+        }
+    }
+
+    void Awake()
+    {
+        initialCameraPosition = transform.position;
+        currentMagnitude = 0;
+    }
+}
